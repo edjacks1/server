d857dafbf3326b3df28919ef64e62a8396c415f4
edjacks1 <xlledjackllx@gmail.com>
2020-09-04 15:59:55 -0500

    asd

--- Controllers/HomeController.py
+++ Controllers/HomeController.py
@@ -1,12 +1,12 @@
 import os
-# import pandas as pd
+import pandas as pd
 
 from Config.config     import db
 from flask_restful     import Resource
 from flask             import request
 from werkzeug.utils    import secure_filename
-# from Services.submit   import allowed_file
-# from Services.csvdb    import csv_to_db
+from Services.submit   import allowed_file
+from Services.csvdb    import csv_to_db
 
 
 class Home(Resource):
@@ -14,10 +14,10 @@ class Home(Resource):
         
         file = request.files['File']
         
-        # if(file and allowed_file(file.filename)):
-        #     filename = secure_filename(file.filename)
-        #     file.save(os.path.join('../Data/', filename))
-        #     csv_to_db(data=pd.read_csv("../Data/data.csv"),db=db)
+        if(file and allowed_file(file.filename)):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('../Data/', filename))
+            csv_to_db(data=pd.read_csv("../Data/data.csv"),db=db)
         
         return {
             'status':True,
